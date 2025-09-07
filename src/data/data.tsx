6fc2d11c653cdfabd923143f8a767eef22d34507
Diego Jossel <diegokell00@gmail.com>
2025-09-07 09:28:35 -0400

    added a file data wich relevant data about restaurant

---
+++ src/data/data.tsx
@@ -0,0 +1,28 @@
+export const data = {
+  name: "100 Montaditos",
+  phone: "123-456-789",
+  email: "l.castro@100montaditosrd.com",
+  Horario: "Todos los días: 11 am a 11 pm.",
+  imstagram: "https://www.instagram.com/100montaditosrd/",
+  tiktok: "https://www.tiktok.com/@100montaditosrd",
+  ubicacion1: [
+    {
+      ubicacion1: "Sucursal Galería 360",
+      link: "https://goo.gl/maps/xyz123",
+    },
+    {
+      ubicacion2: "Sucursal Downtown Center",
+      link: "https://goo.gl/maps/xyz123",
+    },
+  ],
+  delivery: [
+    {
+      servicio1: "Uber Eats ",
+      link: "https://wa.me/123456789?text=Hola,%20quiero%20hacer%20un%20pedido",
+    },
+    {
+      servicio2: "Rappi",
+      link: "https://wa.me/123456789?text=Hola,%20quiero%20hacer%20un%20pedido",
+    },
+  ],
+};
